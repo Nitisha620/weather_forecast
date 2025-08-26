10f273f2b42f9c2d8de04210e0ca4ca0c0e8bfe8
Nitisha620 <nitishajain94@gmail.com>
2025-08-26 22:34:55 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+import 'presentation/home_page.dart';
+import 'styles/app_theme.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Weather Forecast',
+      debugShowCheckedModeBanner: false,
+      theme: AppThemes.themedata,
+      home: const HomePage(),
+    );
+  }
+}
