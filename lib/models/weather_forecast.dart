0e0bca0bcf08173af636c1e972deba4156809c3d
Nitisha620 <nitishajain94@gmail.com>
2025-08-27 23:00:04 +0530

    Added Model classes

---
+++ lib/models/weather_forecast.dart
@@ -0,0 +1,86 @@
+import 'package:weather_forcast/models/current_weather.dart';
+
+class WeatherForecast {
+  String? cod;
+  int? message;
+  int? cnt;
+  List<CurrentWeather>? list;
+  City? city;
+
+  WeatherForecast({this.cod, this.message, this.cnt, this.list, this.city});
+
+  WeatherForecast.fromJson(Map<String, dynamic> json) {
+    cod = json['cod'];
+    message = json['message'];
+    cnt = json['cnt'];
+    if (json['list'] != null) {
+      list = <CurrentWeather>[];
+      json['list'].forEach((v) {
+        list!.add(CurrentWeather.fromJson(v));
+      });
+    }
+    city = json['city'] != null ? City.fromJson(json['city']) : null;
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['cod'] = cod;
+    data['message'] = message;
+    data['cnt'] = cnt;
+    if (list != null) {
+      data['list'] = list!.map((v) => v.toJson()).toList();
+    }
+    if (city != null) {
+      data['city'] = city!.toJson();
+    }
+    return data;
+  }
+}
+
+class City {
+  int? id;
+  String? name;
+  Coord? coord;
+  String? country;
+  int? population;
+  int? timezone;
+  int? sunrise;
+  int? sunset;
+
+  City({
+    this.id,
+    this.name,
+    this.coord,
+    this.country,
+    this.population,
+    this.timezone,
+    this.sunrise,
+    this.sunset,
+  });
+
+  City.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    name = json['name'];
+    coord = json['coord'] != null ? new Coord.fromJson(json['coord']) : null;
+    country = json['country'];
+    population = json['population'];
+    timezone = json['timezone'];
+    sunrise = json['sunrise'];
+    sunset = json['sunset'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['id'] = id;
+    data['name'] = name;
+    if (coord != null) {
+      data['coord'] = coord!.toJson();
+    }
+    data['country'] = country;
+    data['population'] = population;
+    data['timezone'] = timezone;
+    data['sunrise'] = sunrise;
+    data['sunset'] = sunset;
+    return data;
+  }
+}
