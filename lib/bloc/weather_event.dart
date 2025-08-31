20bcaf371ba66eb8cb1d6441ef48d5750e39719c
Nitisha620 <nitishajain94@gmail.com>
2025-08-31 22:40:31 +0530

    Added new events

--- lib/bloc/weather_event.dart
+++ lib/bloc/weather_event.dart
@@ -2,9 +2,21 @@ part of 'weather_bloc.dart';
 
 class WeatherEvent {}
 
-class FetchCurrentLocation extends WeatherEvent{}
+class FetchCurrentLocation extends WeatherEvent {
+  String? cityName;
+  FetchCurrentLocation({this.cityName});
+}
 
 class FetchCurrentWeather extends WeatherEvent {
   Position? position;
   FetchCurrentWeather({this.position});
 }
+
+class ToggleMapLayer extends WeatherEvent {
+  final String layer;
+  ToggleMapLayer(this.layer);
+}
+
+class SelectMarker extends WeatherEvent {}
+
+class DeselectMarker extends WeatherEvent {}
