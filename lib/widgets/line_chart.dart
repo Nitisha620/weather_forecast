053693b0fff89bbd32e23bf6091ef47751edd832
Nitisha620 <nitishajain94@gmail.com>
2025-08-31 22:43:15 +0530

    Added dynamic data

--- lib/widgets/line_chart.dart
+++ lib/widgets/line_chart.dart
@@ -1,20 +1,62 @@
 import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:weather_forcast/styles/app_text_style.dart';
+import 'package:weather_forcast/utils/icon_util.dart';
 
-class TempChart extends StatelessWidget {
-  final List<String> days = ['Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
-  final List<double> minTemps = [27, 26, 29, 28, 25];
-  final List<double> maxTemps = [33, 32, 35, 34, 31];
-  final List<String> weatherIcons = ["☀️", "🌧️", "☁️", "⛈️", "❄️"];
+class TempChart extends StatefulWidget {
+  final List<Map<String, dynamic>> forecasetInfo;
 
-  TempChart({super.key});
+  const TempChart({required this.forecasetInfo, super.key});
+
+  @override
+  State<TempChart> createState() => _TempChartState();
+}
+
+class _TempChartState extends State<TempChart> {
+  List<Map<String, dynamic>> forecasetInfo = [];
+  List<double> minTemps = [];
+  List<double> maxTemps = [];
+
+  @override
+  void initState() {
+    super.initState();
+    forecasetInfo = widget.forecasetInfo;
+    for (var i in forecasetInfo) {
+      minTemps.add(i['min']);
+      maxTemps.add(i['max']);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return LineChart(
       LineChartData(
         gridData: FlGridData(show: true, drawVerticalLine: false),
+        lineTouchData: LineTouchData(
+          touchTooltipData: LineTouchTooltipData(
+            getTooltipColor: (touchedSpot) =>
+                Colors.white.withAlpha(51), // background
+
+            tooltipPadding: const EdgeInsets.all(10),
+            tooltipMargin: 8,
+            getTooltipItems: (touchedSpots) {
+              return touchedSpots.map((touchedSpot) {
+                final textStyle = AppTextStyle.font14Manrope;
+
+                String label;
+                if (touchedSpot.barIndex == 0) {
+                  label = "Max: ${touchedSpot.y.toStringAsFixed(1)}°C";
+                } else if (touchedSpot.barIndex == 1) {
+                  label = "Min: ${touchedSpot.y.toStringAsFixed(1)}°C";
+                } else {
+                  label = "Value: ${touchedSpot.y.toStringAsFixed(1)}";
+                }
+                return LineTooltipItem(label, textStyle);
+              }).toList();
+            },
+          ),
+        ),
         titlesData: FlTitlesData(
           bottomTitles: AxisTitles(
             sideTitles: SideTitles(
@@ -22,20 +64,25 @@ class TempChart extends StatelessWidget {
               showTitles: true,
               getTitlesWidget: (value, meta) {
                 int index = value.toInt();
-
-                // only show labels if it's a valid index
-                if (value % 1 == 0 && index >= 0 && index < days.length) {
+                if (value % 1 == 0 &&
+                    index >= 0 &&
+                    index < widget.forecasetInfo.length) {
+                  final style = getWeatherIconStyle(
+                    widget.forecasetInfo[index]['icon'],
+                  );
                   return Column(
                     children: [
                       Expanded(
                         child: Text(
-                          days[index],
+                          DateFormat('EEE').format(
+                            DateTime.parse(
+                              widget.forecasetInfo[index]['date'].toString(),
+                            ),
+                          ),
                           style: AppTextStyle.font12Grey,
                         ),
                       ),
-                      Expanded(
-                        child: Text(weatherIcons[index % weatherIcons.length]),
-                      ),
+                      Icon(style.icon, color: style.color),
                     ],
                   );
                 }
@@ -59,15 +106,11 @@ class TempChart extends StatelessWidget {
           topTitles: AxisTitles(sideTitles: SideTitles(showTitles: false)),
         ),
         borderData: FlBorderData(show: false),
-        // ✅ Fix range to match 5 days
         minX: 0,
-        maxX: (days.length - 1).toDouble(),
-
-        // ✅ dynamic range for temps
+        maxX: (widget.forecasetInfo.length - 1).toDouble(),
         minY: (minTemps.reduce((a, b) => a < b ? a : b)) - 2,
         maxY: (maxTemps.reduce((a, b) => a > b ? a : b)) + 2,
         lineBarsData: [
-          // Max temp line (Red)
           LineChartBarData(
             spots: List.generate(
               maxTemps.length,
@@ -75,11 +118,10 @@ class TempChart extends StatelessWidget {
             ),
             isCurved: true,
             color: Colors.red,
-            barWidth: 3,
+            barWidth: 1.5,
             isStrokeCapRound: true,
             dotData: FlDotData(show: true),
           ),
-          // Min temp line (Blue)
           LineChartBarData(
             spots: List.generate(
               minTemps.length,
@@ -87,7 +129,7 @@ class TempChart extends StatelessWidget {
             ),
             isCurved: true,
             color: Colors.blue,
-            barWidth: 3,
+            barWidth: 1.5,
             isStrokeCapRound: true,
             dotData: FlDotData(show: true),
           ),
