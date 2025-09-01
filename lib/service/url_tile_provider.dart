ed902553fc7991268002cc15a68fb2de9b7c075e
Nitisha620 <nitishajain94@gmail.com>
2025-09-01 10:38:15 +0530

    (Refactor) Code Cleaning

--- lib/service/url_tile_provider.dart
+++ lib/service/url_tile_provider.dart
@@ -1,4 +1,5 @@
 import 'dart:ui' as ui;
+import 'package:flutter/foundation.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:http/http.dart' as http;
 
@@ -12,22 +13,17 @@ class UrlTileProvider implements TileProvider {
   @override
   Future<Tile> getTile(int x, int y, int? zoom) async {
     if (zoom == null) {
-      // generate transparent tile
       return _emptyTile();
     }
-
     final url = getTileUrl(x, y, zoom);
-
     try {
       final response = await http.get(Uri.parse(url));
       if (response.statusCode == 200) {
         return Tile(width, height, response.bodyBytes);
       }
     } catch (e) {
-      print("Tile load error: $e");
+      debugPrint("Tile load error: $e");
     }
-
-    // fallback to transparent tile
     return _emptyTile();
   }
 
@@ -42,7 +38,6 @@ class UrlTileProvider implements TileProvider {
     final picture = recorder.endRecording();
     final img = await picture.toImage(width, height);
     final byteData = await img.toByteData(format: ui.ImageByteFormat.png);
-
     return Tile(width, height, byteData!.buffer.asUint8List());
   }
 }
