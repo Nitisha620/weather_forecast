41ceb032960a9d8965ab7007145b5ed676fd3f64
Nitisha620 <nitishajain94@gmail.com>
2025-09-01 10:36:07 +0530

    Added url end points

---
+++ lib/service/url_enpoints.dart
@@ -0,0 +1,6 @@
+class UrlEndPoints {
+  static const baseUrl = "https://api.openweathermap.org";
+  static const geo = "$baseUrl/geo/1.0/direct";
+  static const currentWeather = "$baseUrl/data/2.5/weather";
+  static const forecast = "$baseUrl/data/2.5/forecast";
+}
